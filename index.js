06167fda2f0e1bc64057b87fbb120153de400a15
Hen Weaver <henweaver@icloud.com>
2023-10-23 18:08:01 -0400

    Moved the notes over to a readme

--- index.js
+++ index.js
@@ -1,73 +1,4 @@
-// let pokemon = ['Pikachu','Charmander','Squirtle'];
-// let question = `Which of the following do you select?`
 
-// let choice = getSelection(question,pokemon);
-// console.log(choice);
-
-// function getSelection(question,options) {
-
-//     let string = "";
-//     for (let x = 0; x < options.length; x++) {
-//         string += '\n' + `${x+1}: ${options[x]}`;
-//     }
-
-//     let choice = prompt(`${question} \n ${string}`);
-//     return options[choice-1]
-// }
-
-
- /* What we need    
-    Animal objects
-        That would be subdivided into 
-
-
-        Classes:
-            Habitats
-                Two Land Habitats
-                Two Aquatic Habitats
-
-
-            Known Animal Traits
-
-            Each animal object would have a set of traits
-
-            Does it breath air?
-
-
-
-
-
-            Switching it up - kick the zoo
-
-            Putting them in the wild
-
-            Don't care if they eat each other
-
-            Just check if they would suit in the enviroment
-
-            We have two objects affecting each other
-
-            We have a clock keeping track of the years. 
-
-            Simply checking for strings
-
-            Present the user with a template so they can put in whtat they obverve about the animale
-            We will see what matches the best 
-
-        The Two Habitats
-
-            The Jungle
-
-            The Savanah
-
-
-            
-
-
-
-
- 
- */
 
 class Zoo {
     constructor(biomes) {
@@ -160,24 +91,4 @@ document.addEventListener("DOMContentLoaded", function() {
         document.getElementById('animalList').innerHTML = output;
     });
 });
-
-
-
-
-//Takes one animal object
-// For each biome in biomes 
-    // Test the animal against a certain set of properties
-    // 
-    // Test it against isForested and prefTemp
-    //Keep track of the number of matches
-    //Output the biome with the most matches
-
-
-// Now we need to create a function so that when the user puts in the traits it will 
-// first create the animal 
-// and then check the animal
-// We need a way to iterate through the biomes
-// create a for biomes in biomes loop and check the properties
-
-
 
